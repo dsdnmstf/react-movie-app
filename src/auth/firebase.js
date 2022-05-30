f573569ccf2e329c4db08731bff11da57cf6603e
musrafa <dsdnmstf@gmail.com>
2022-05-30 23:44:24 +0200

    firebas config added

---
+++ src/auth/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD04TMhtOHKGVZGiz0Ep8ILGPF84-3A3PQ",
+  authDomain: "react-movie-app-1c3bf.firebaseapp.com",
+  projectId: "react-movie-app-1c3bf",
+  storageBucket: "react-movie-app-1c3bf.appspot.com",
+  messagingSenderId: "800219125223",
+  appId: "1:800219125223:web:8e664e4771972ec5a99942",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
